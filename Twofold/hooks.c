21e6c65e23e2031254451031eb371a0b3ea71c85
Spitfire_x86 <33195187+spitfirex86@users.noreply.github.com>
2024-01-23 13:51:32 +0100

    disable PTC checks at level load

--- Twofold/hooks.c
+++ Twofold/hooks.c
@@ -27,15 +27,65 @@ void fn_vInitGameLoop( void )
 	HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(1));
 	SendMessage(hWnd, WM_SETICON, ICON_BIG, (LPARAM)hIcon);
 	SendMessage(hWnd, WM_SETICON, ICON_SMALL, (LPARAM)hIcon);
+	LOG_InfoVerbose("fn_vInitGameLoop : applied icon patch");
 
 	GAM_fn_vInitGameLoop();
 }
 
+void NeutralizePTC_SuperObj( HIE_tdstSuperObject *hSuperObj )
+{
+	HIE_tdstStandardGame *pStdGame = HIE_M_hSuperObjectGetStdGame(hSuperObj);
+	pStdGame->ulCustomBits |= Std_C_CustBit_NoAI;
+	pStdGame->ulSaveCustomBits |= Std_C_CustBit_NoAI;
+	pStdGame->ucMiscFlags |= Std_C_MiscFlag_DesactivateAtAll;
+	pStdGame->ucMiscFlags &= ~Std_C_MiscFlag_Activable;
+	pStdGame->ucSaveMiscFlags |= Std_C_MiscFlag_DesactivateAtAll;
+	pStdGame->ucSaveMiscFlags &= ~Std_C_MiscFlag_Activable;
+	pStdGame->eInitFlagWhenDeadOrTaken = Std_C_OI_NeverBackWhenTaken;
+	pStdGame->eInitFlagWhenOutOfZone = Std_C_OI_NeverBackWhenTaken;
+}
+
+void NeutralizePTC( void )
+{
+	HIE_tdxObjectType lType1 = HIE_fn_lFindModelTypeByName("DS1_GEN_PTC_GenCKS");
+	HIE_tdxObjectType lType2 = HIE_fn_lFindModelTypeByName("DS1_GEN_PTC_GenBigFile");
+	LOG_InfoVerbose("PTC : model types: DS1_GEN_PTC_GenCKS = %d, DS1_GEN_PTC_GenBigFile = %d", lType1, lType2);
+
+	if ( lType1 == Std_C_InvalidObjectType || lType2 == Std_C_InvalidObjectType )
+		return;
+
+	HIE_tdstSuperObject *hSuperObj;
+	LST_M_DynamicForEach(*GAM_g_p_stDynamicWorld, hSuperObj)
+	{
+		if ( HIE_M_bSuperObjectIsActor(hSuperObj)
+			&& ( HIE_M_hSuperObjectGetStdGame(hSuperObj)->lObjectModelType == lType1
+				|| HIE_M_hSuperObjectGetStdGame(hSuperObj)->lObjectModelType == lType2 ) )
+		{
+			NeutralizePTC_SuperObj(hSuperObj);
+			LOG_InfoVerbose("PTC : Found and neutralized '%s' (%p)", HIE_fn_szGetObjectPersonalName(hSuperObj), hSuperObj);
+		}
+	}
+	LST_M_DynamicForEach(*GAM_g_p_stInactiveDynamicWorld, hSuperObj)
+	{
+		if ( HIE_M_bSuperObjectIsActor(hSuperObj)
+			&& ( HIE_M_hSuperObjectGetStdGame(hSuperObj)->lObjectModelType == lType1
+				|| HIE_M_hSuperObjectGetStdGame(hSuperObj)->lObjectModelType == lType2 ) )
+		{
+			NeutralizePTC_SuperObj(hSuperObj);
+			LOG_InfoVerbose("PTC : Found and neutralized '%s' (%p) (inactive)", HIE_fn_szGetObjectPersonalName(hSuperObj), hSuperObj);
+		}
+	}
+}
+
 void fn_vInitLevelLoop( void )
 {
-	g_bInMainMenu = !_stricmp(GAM_fn_p_szGetLevelName(), "menu");
+	char const *szLevelName = GAM_fn_p_szGetLevelName();
+	g_bInMainMenu = !_stricmp(szLevelName, "menu");
+	LOG_InfoVerbose("fn_vInitLevelLoop : current level is '%s'", szLevelName);
 
 	GAM_fn_vInitLevelLoop();
+
+	NeutralizePTC();
 }
 
 void fn_vAffiche( void *pContext )
